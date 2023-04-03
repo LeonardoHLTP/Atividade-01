11f88d22c1a38a6520efd5171399d47f6e20ece8
Leonardo <126120062+Bigasx@users.noreply.github.com>
2023-04-03 20:42:36 -0300

    Update atividade01.py

---
+++ atividade01.py
@@ -0,0 +1,12 @@
+nome = "Leonardo"
+sobrenome = "Torres"
+idade = 28
+altura = 1.85
+peso = 75
+maior_de_idade = idade >= 18
+
+print("Nome completo:", nome, sobrenome)
+print("Idade:", idade)
+print("Altura:", altura)
+print("Peso:", peso)
+print("Maior de idade:", maior_de_idade)
